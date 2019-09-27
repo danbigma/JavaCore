d48738dda99598611cc734ffd3158fdfd3cd5512
Danylo Bigma <daniil.bigma@gmail.com>
2019-09-27 12:48:45 +0200

    add new methods

--- src/com/experiment/string/DynamicString.java
+++ src/com/experiment/string/DynamicString.java
@@ -4,18 +4,57 @@ public class DynamicString {
 
 	public static void main(String[] args) {
 		
-		constructDynamicString();
+		System.out.println("StringNormal-------------");
+		constructDynamicStringNormal();
+		System.out.println("-------------------------");
+		
+		System.out.println("StringBuffer-------------");		
+		constructDynamicStringBuffer();
+		System.out.println("-------------------------");
+		
+		System.out.println("StringBuilder------------");
+		constructDynamicStringBuilder();
+		System.out.println("-------------------------");
 
 	}
 
-	public static void constructDynamicString() {
+	public static void constructDynamicStringNormal() {
 		
 		long startTime = System.currentTimeMillis();
-		// String s = "test";
-		StringBuffer s = new StringBuffer("test");
-		// StringBuilder s = new StringBuilder("test");
+		
+		String s = "test";
+		
 		for (int i = 0; i < 100000; i++) {
-			// s += "concat";
+			s += "concat";
+		}
+
+		long endTime = System.currentTimeMillis();
+
+		System.out.println("Concat time ====== " + (endTime - startTime));
+	}
+	
+	public static void constructDynamicStringBuffer() {
+		
+		long startTime = System.currentTimeMillis();
+		
+		StringBuffer s = new StringBuffer("test");
+
+		for (int i = 0; i < 100000; i++) {
+			s.append("concat");
+		}
+
+		long endTime = System.currentTimeMillis();
+
+		System.out.println("Concat time ====== " + (endTime - startTime));
+	}
+	
+	public static void constructDynamicStringBuilder() {
+		
+		long startTime = System.currentTimeMillis();
+		
+		StringBuilder s = new StringBuilder("test");
+		
+		for (int i = 0; i < 100000; i++) {
 			s.append("concat");
 		}
 
