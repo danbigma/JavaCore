543744f549c2d2322d567c884530c4db8f32690e
Danylo Bigma <daniil.bigma@gmail.com>
2019-12-27 09:50:05 +0100

    userHomeFolder

--- src/com/experiment/properties/NameOS.java
+++ src/com/experiment/properties/NameOS.java
@@ -2,13 +2,18 @@ package com.experiment.properties;
 
 public class NameOS {
 
-	private final static String osNamePropertie = "os.name";
+	private final static String osName = "os.name";
+	private final static String userHome = "user.home";
 
 	public static void main(String[] args) {
 
-		String useOSname = System.getProperty(osNamePropertie);
+		String useOSname = System.getProperty(osName);
+		String userHomeFolder = System.getProperty(userHome);
 
 		System.out.println("You use - " + useOSname);
+		System.out.println();
+		System.out.println("User home folder - " + userHomeFolder);
+		System.out.println();
 
 		System.getProperties().list(System.out);
 
