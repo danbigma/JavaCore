58959211316ea98e4fa18489e4c93c09569f87cb
Danylo Bigma <daniil.bigma@gmail.com>
2019-12-27 11:50:44 +0100

    add properties user.dir

--- src/com/experiment/properties/NameOS.java
+++ src/com/experiment/properties/NameOS.java
@@ -4,16 +4,19 @@ public class NameOS {
 
 	private final static String osName = "os.name";
 	private final static String userHome = "user.home";
+	private final static String userDir = "user.dir";
 
 	public static void main(String[] args) {
 
 		String useOSname = System.getProperty(osName);
 		String userHomeFolder = System.getProperty(userHome);
+		String userDirectory = System.getProperty(userDir);
 
 		System.out.println("You use - " + useOSname);
 		System.out.println();
 		System.out.println("User home folder - " + userHomeFolder);
 		System.out.println();
+		System.out.println("User dir - " + userDirectory);
 
 		System.getProperties().list(System.out);
 
