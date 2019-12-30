ad3177723bda3d6b3d4e48af0018b6cc34e27726
Danylo Bigma <daniil.bigma@gmail.com>
2019-12-30 17:39:52 +0100

    new prop

---
+++ src/com/test/properties/CrunchifyReadConfigMain.java
@@ -0,0 +1,10 @@
+package com.test.properties;
+
+import java.io.IOException;
+
+public class CrunchifyReadConfigMain {
+	public static void main(String[] args) throws IOException {
+		CrunchifyGetPropertyValues properties = new CrunchifyGetPropertyValues();
+		properties.getPropValues();
+	}
+}
