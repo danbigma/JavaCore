b2cba88d8929ec496c6360720bd89dfd2b175ddd
Danylo Bigma <daniil.bigma@gmail.com>
2020-01-07 16:58:06 +0100

    testing...

--- src/com/test/testclass/ClassTesting.java
+++ src/com/test/testclass/ClassTesting.java
@@ -4,6 +4,24 @@ public class ClassTesting {
 
 	public ClassTesting() {
 		System.out.println("Start empty constructor!");
+		System.out.println("------------------------");
+	}
+	
+	public ClassTesting(int a, int b) {
+		System.out.println("Start constructor with Parameters!");
+		System.out.println(a+b);
+		System.out.println("------------------------");
+	}
+	
+	// private method 
+	private int sum(int a, int b) {
+		return a+b;
+	}
+	
+	// public method
+	public void publicMethod() {
+		System.out.println("Public method!");
+		System.out.println("------------------------");
 	}
 
 }
--- src/com/test/testclass/MainTest.java
+++ src/com/test/testclass/MainTest.java
@@ -5,6 +5,10 @@ public class MainTest {
 	public static void main(String[] args) {
 		
 		ClassTesting firstclass = new ClassTesting();
+		
+		firstclass.publicMethod();
+		
+		ClassTesting secondclass = new ClassTesting(5, 5);
 
 	}
 
