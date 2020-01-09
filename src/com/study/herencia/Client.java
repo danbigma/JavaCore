c8603770d30ac53613f5be010da5cbbf2b2fadb1
Danylo Bigma <daniil.bigma@gmail.com>
2020-01-09 13:26:29 +0100

    add new class Client

---
+++ src/com/study/herencia/Client.java
@@ -0,0 +1,53 @@
+package com.study.herencia;
+
+import java.util.Date;
+
+public class Client extends Person {
+
+	private int idClient;
+	private String namecompany;
+	private Date registered;
+	private static int counter;
+
+	
+	public Client() {
+		super();
+	}
+
+	public Client(String firstname, String lastname, String gender, Date registered, String namecompany) {
+		super(firstname, lastname, gender);
+		this.idClient = ++counter;
+		this.namecompany = namecompany;
+		this.registered = registered;
+	}
+	
+	public String getNamecompany() {
+		return namecompany;
+	}
+	
+	public void setNamecompany(String namecompany) {
+		this.namecompany = namecompany;
+	}
+	
+	public Date getRegistered() {
+		return registered;
+	}
+	
+	public void setRegistered(Date registered) {
+		this.registered = registered;
+	}
+	
+	public int getIdClient() {
+		return idClient;
+	}
+
+	public static int getCounter() {
+		return counter;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + "Client [idClient=" + idClient + ", namecompany=" + namecompany + ", registered=" + registered + "]";
+	}
+	
+}
