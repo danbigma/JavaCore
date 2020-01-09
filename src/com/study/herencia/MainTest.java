b8c37efd67b683f77e517a112f71dc9069c505dd
Danylo Bigma <daniil.bigma@gmail.com>
2020-01-09 13:14:46 +0100

    testing...

--- src/com/study/herencia/MainTest.java
+++ src/com/study/herencia/MainTest.java
@@ -6,10 +6,13 @@ public class MainTest {
 		Employee employee = new Employee("Danylo", "Bigma", "man", 100000);
 		System.out.println(employee.toString());
 		
-		Employee employee2 = new Employee("Pedro", "Flores", "man", 100000);
+		Employee employee2 = new Employee("Pedro", "Diez", "man", 100000);
 		System.out.println(employee2.toString());
 		
 		Employee employee3 = new Employee("Pablo", "Escobar", "man", 10000000000L);
+		System.out.println(employee3.toString());
+		
+		employee3.setSalary(200);
 		System.out.println(employee3.toString());
 	}
 
