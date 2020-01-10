d80972690b858c3d9198aeb720423cd17f51adaf
db90 <daniil.bigma@gmail.com>
2020-01-11 00:50:28 +0100

    modif enum

--- src/com/test/enums/Dias.java
+++ src/com/test/enums/Dias.java
@@ -1,5 +1,27 @@
 package com.test.enums;
 
 public enum Dias {
-	LUNES, MARTES, MIERCOLE, JUEVES, VIERNES, SABADO, DOMINGO;
+	LUNES(1, "Lunes"),
+	MARTES(2, "Martes"),
+	MIERCOLE(3, "Miercoles"),
+	JUEVES(4, "Jueves"),
+	VIERNES(5, "Viernes"),
+	SABADO(6, "Sabado"),
+	DOMINGO(7, "Domingo");
+	
+	private final int numDiaDeLaSemana;
+	private final String diaDeLaSemana;
+
+	Dias(int i, String name) {
+		this.numDiaDeLaSemana = i;
+		this.diaDeLaSemana = name;
+	}
+
+	public int getNumDiaDeLaSemana() {
+		return numDiaDeLaSemana;
+	}
+
+	public String getDiaDeLaSemana() {
+		return diaDeLaSemana;
+	}
 }
--- src/com/test/enums/Main.java
+++ src/com/test/enums/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println(Dias.LUNES);	
+		System.out.println(Dias.MARTES.getDiaDeLaSemana());	
 
 	}
 
