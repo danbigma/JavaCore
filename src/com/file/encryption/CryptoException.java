3f61e6dfcd23358420a3a2601f1b2ed3b3162813
Danylo Bigma <daniil.bigma@gmail.com>
2020-01-15 22:12:23 +0100

    commit tonto

--- src/com/file/encryption/CryptoException.java
+++ src/com/file/encryption/CryptoException.java
@@ -4,8 +4,7 @@ public class CryptoException extends Exception {
 
 	private static final long serialVersionUID = 1L;
 
-	public CryptoException() {
-	}
+	public CryptoException() {}
 
 	public CryptoException(String message, Throwable throwable) {
 		super(message, throwable);
