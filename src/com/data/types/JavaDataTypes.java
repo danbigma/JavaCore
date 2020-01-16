abcf9e51c294e21b32324b920204a6b2fc967aa9
Danylo Bigma <daniil.bigma@gmail.com>
2020-01-16 15:15:41 +0100

    I dont now

--- src/com/data/types/JavaDataTypes.java
+++ src/com/data/types/JavaDataTypes.java
@@ -22,7 +22,7 @@ public class JavaDataTypes {
 		float myFloatNum = 5.99f;    // Floating point number
 		char myLetter = 'D';         // Character
 		boolean myBool = true;       // Boolean
-		String myText = "Hello";     // String
+		String myText = "Hello";     // String 
 		
 		
 		System.out.println("int - " + myNum);
