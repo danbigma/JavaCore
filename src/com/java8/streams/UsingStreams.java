b749025ed42c4720d6db097dc77a0efb8fd9203a
db90 <daniil.bigma@gmail.com>
2020-01-17 19:48:56 +0100

    5

--- src/com/java8/streams/UsingStreams.java
+++ src/com/java8/streams/UsingStreams.java
@@ -6,7 +6,6 @@ public class UsingStreams {
 
 	public static void main(String[] args) throws java.lang.Exception {
 		Stream<String> fruitStream = Stream.of("apple", "banana", "pear", "kiwi", "orange");
-		
 		fruitStream.filter(s -> s.contains("a")).map(String::toUpperCase).sorted().forEach(System.out::println);
 	}
 
