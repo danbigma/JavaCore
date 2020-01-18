606bbe71160ec8be9b245b901d3ce29e72eebcea
db90 <daniil.bigma@gmail.com>
2020-01-18 22:25:17 +0100

    fuck this commit

--- src/com/test/string/ConcatTest.java
+++ src/com/test/string/ConcatTest.java
@@ -4,6 +4,7 @@ package com.test.string;
 public class ConcatTest {
 
 	public static void main(String[] args) {
+		
 		long startTime = System.currentTimeMillis();
 		StringBuffer sb = new StringBuffer("Java");
 		for (int i = 0; i < 10000; i++) {
@@ -11,6 +12,7 @@ public class ConcatTest {
 		}
 		System.out.println("Time taken by StringBuffer: " + (System.currentTimeMillis() - startTime) + "ms");
 		
+		// StringBuilder is better!
 		startTime = System.currentTimeMillis();
 		StringBuilder sb2 = new StringBuilder("Java");
 		for (int i = 0; i < 10000; i++) {
