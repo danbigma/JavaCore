c43e0903a44b1ed8812e714bb30cb1bec6a6be8f
danbigma <danylo.bigma@everis.com>
2020-07-29 12:39:13 +0200

    new comm

--- src/com/test/json/ParsingJson.java
+++ src/com/test/json/ParsingJson.java
@@ -1,35 +1,40 @@
 package com.test.json;
 
+import java.io.File;
+import java.util.Iterator;
 import java.util.List;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 public class ParsingJson {
 
 	public static void main(String[] args) throws Exception {
 		
-		String filename0 = "/Users/daniilbigma/dev/workspace_Eclipse/JavaCore/src/com/experiment/json/example--2.json";
+		File fileJSON = new File("data/countries.json");
 		
-		JSONObject jsonObject = (JSONObject) UtilJSON.readJsonSimpleDemo(filename0);
-		System.out.println(jsonObject);
+		if (fileJSON.exists()) {
+			JSONArray jsonObject = (JSONArray) UtilJSON.readJsonSimpleDemo(fileJSON.getAbsolutePath());
+			
+			for (int i = 0; jsonObject.size() > i; i++)
+                System.out.println(jsonObject.get(i));
+		} else {
+			System.out.println("File does't exist!");
+		}
 		
 		//parsingJSONtoObject(filename0);		
 
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public static void parsingJSONtoObject(String filename) throws Exception {
-		
-	    JSONObject jsonObject = (JSONObject) UtilJSON.readJsonSimpleDemo(filename);
-	    Person ben = new Person(
-	                (String) jsonObject.get("name"),
-	                Integer.valueOf(jsonObject.get("age").toString()),
-	                (Boolean) jsonObject.get("isMarried"),
-	                (List<String>) jsonObject.get("hobbies"),
-	                (List<Person>) jsonObject.get("kids"));
 
-	    System.out.println(ben);
+		JSONObject jsonObject = (JSONObject) UtilJSON.readJsonSimpleDemo(filename);
+		Person ben = new Person((String) jsonObject.get("name"), Integer.valueOf(jsonObject.get("age").toString()),
+				(Boolean) jsonObject.get("isMarried"), (List<String>) jsonObject.get("hobbies"),
+				(List<Person>) jsonObject.get("kids"));
+
+		System.out.println(ben);
 	}
-
 
 }
