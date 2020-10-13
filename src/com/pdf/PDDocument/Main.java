a38f1ae255c2804ccb16248edbe512f96aa54c01
danbigma <danylo.bigma@everis.com>
2020-10-13 13:04:49 +0200

    pdf

--- src/com/pdf/PDDocument/Main.java
+++ src/com/pdf/PDDocument/Main.java
@@ -1,5 +1,6 @@
 package com.pdf.PDDocument;
 
+import java.io.File;
 import java.io.IOException;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
@@ -8,30 +9,26 @@ import org.apache.pdfbox.pdmodel.PDPage;
 public class Main {
 
 	public static void main(String[] args) {
-
 		PDDocument doc = new PDDocument();
-
 		try {
 			// Creating PDF document object
-
 			for (int i = 0; i < 5; i++) {
 				// Creating a blank page
 				PDPage blankPage = new PDPage();
-
 				// Adding the blank page to the document
 				doc.addPage(blankPage);
 			}
-
 			// Saving the document
 			doc.save("C:\\dev\\GIT_local\\JavaCore\\data\\blank.pdf");
-
-			System.out.println("PDF created");
+			
+			File filePDF = new File("C:\\dev\\GIT_local\\JavaCore\\data\\blank.pdf");
+			
+			System.out.println("PDF created wiht name: " + filePDF.getName());
 			// Closing the document
 			doc.close();
 		} catch (IOException e) {
 			System.out.println("IOException " + e.getMessage());
 		}
-
 	}
 
 }
