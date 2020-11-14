930ab4b8810394ef1a87419aa6137cc189ecc923
danbigma <daniil.bigma@gmail.com>
2020-11-14 18:47:00 +0100

    add lorem

--- src/com/io/file/CopyFileByte.java
+++ src/com/io/file/CopyFileByte.java
@@ -6,26 +6,31 @@ import java.io.IOException;
 import java.io.File;
 
 public class CopyFileByte {
-	
-	private static final String INPUT = "data//input.txt";
-	private static final String OUTPUT = "data//output.txt";
-	
-	public static void main(String args[]) throws IOException {
+
+	private static final String FILE_SEPARATOR = "file.separator";
+	private static final String USER_DIR = "user.dir";
+
+	private static final String INPUT = System.getProperty(USER_DIR) + System.getProperty(FILE_SEPARATOR) + "data"
+			+ System.getProperty(FILE_SEPARATOR) + "input.txt";
+	private static final String OUTPUT = System.getProperty(USER_DIR) + System.getProperty(FILE_SEPARATOR) + "data"
+			+ System.getProperty(FILE_SEPARATOR) + "output.txt";
+
+	public static void main(String[] args) throws IOException {
 		FileReader in = null;
 		FileWriter out = null;
-		
-		String[] fileNames = {INPUT, OUTPUT};
-		
+
+		String[] fileNames = { INPUT, OUTPUT };
+
 		for (String f : fileNames) {
 			File newfile = new File(f);
-			if(newfile.exists()) {
+			if (newfile.exists()) {
 				newfile.delete();
 				newfile.createNewFile();
 			} else {
 				newfile.createNewFile();
 			}
 		}
-		
+
 		try {
 			in = new FileReader(new File(fileNames[0]));
 			out = new FileWriter(new File(fileNames[1]));
