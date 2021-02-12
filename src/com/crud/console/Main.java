ed9318ca7537abc1a3ed428cf0e840f9a4b5a34d
danbigma <danbigma@everis.com>
2021-02-12 23:01:58 +0100

    format

--- src/com/crud/console/Main.java
+++ src/com/crud/console/Main.java
@@ -22,31 +22,31 @@ public class Main {
 		do {
 			menu.getMenu();
 			int action = scanner.nextInt();
-			
+
 			switch (action) {
-				case 1:
-					function.addNewUser();
-					break;
-				case 2:
-					function.listUsers();
-					break;
-				case 3:
-					function.deleteUser();
-					break;
-				case 4:
-					function.modifUser();
-					break;
-				case 5:
-					System.out.println("Are you sure y/n ?");
-					exit = scanner.next();
-					if ("y".equals(exit)) {
-						start = false;
-					} else {
-						start = true;
-					}
-					break;
-				default:
-					break;
+			case 1:
+				function.addNewUser();
+				break;
+			case 2:
+				function.listUsers();
+				break;
+			case 3:
+				function.deleteUser();
+				break;
+			case 4:
+				function.modifUser();
+				break;
+			case 5:
+				System.out.println("Are you sure y/n ?");
+				exit = scanner.next();
+				if ("y".equals(exit)) {
+					start = false;
+				} else {
+					start = true;
+				}
+				break;
+			default:
+				break;
 			}
 
 		} while (start);
